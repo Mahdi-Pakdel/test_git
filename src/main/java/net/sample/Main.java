71c0900dfbd8c8f4ec5117335ebdd779942c91c1
MahdiPakdel <mahdipakdel85@gmail.com>
2026-02-18 19:27:36 +0330

    reza test

--- src/main/java/net/sample/Main.java
+++ src/main/java/net/sample/Main.java
@@ -5,6 +5,7 @@ public class Main {
 
 
         System.out.printf("Hello and welcome!");
+        System.out.printf("test reza");
 
         for (int i = 1; i <= 5; i++) {
             System.out.println("i = " + i);
